3598bf1ad11f88b5007b219135b73fe3b6f77ae5
Bachir Bekkaye <bachirbk@gmail.com>
2024-12-06 06:03:30 -0800

    Add project files.

---
+++ Neighborhood.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAddinBotcamp
+{
+    public class Neighborhood
+    {
+        public string Name { get; set; }
+        public string City { get; set;}
+        public string State { get; set; }
+
+        public List<Building> BuildingList { get; set; }
+
+
+        public Neighborhood(string _name, string _city, string _state, List<Building> _buildingList)
+        {
+            Name = _name;
+            City = _city;
+            State = _state;
+            BuildingList = _buildingList;
+        }
+
+        public int GetBuildingCount()
+        {
+            return BuildingList.Count;
+        }
+
+    }
+
+    // 1. Create a class called Building
+    public class Building
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NumbersOfFloors { get; set; }
+        public double Area { get; set; }
+
+        // 3. Create a constructor for the Building class
+        public Building(string _name, string _address, int _numberOfFloors, double _area)
+        {
+            Name = _name;
+            Address = _address;
+            NumbersOfFloors = _numberOfFloors;
+            Area = _area;
+
+        }
+    }
+}
---
+++ cmdSkills03.cs
@@ -0,0 +1,97 @@
+﻿using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Structure;
+
+namespace RevitAddinBotcamp
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdSkills03 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // 2. Create an instance of the Building class
+
+            Building building1 = new Building("Big Office Building", "10 Main Street",
+                10, 150_000);
+            Building building2 = new Building("Fancy Hotel", "15 Main Street",
+                15, 200_000);
+
+            List<Building> buildings = new List<Building> { building1, building2 };
+
+            buildings.Add(new Building("Hospital", "20 Main Street", 20, 350_000));
+
+            buildings.Add(new Building("Giant Store", "30 Main Street", 5, 40_000));
+
+
+            // 4. Create an instance of the Neighborhood class
+            Neighborhood downtown = new Neighborhood("Downtown", "New York", "NY", buildings);
+
+            TaskDialog.Show("Test", $"There are {downtown.GetBuildingCount()} buildings " +
+                                    $"in the {downtown.Name} neighborhood.");
+
+            // 5. Work with rooms.
+            FilteredElementCollector roomCollector = new FilteredElementCollector(doc);
+            roomCollector.OfCategory(BuiltInCategory.OST_Rooms);
+
+            Room curRoom = roomCollector.First() as Room;
+
+            // Get the room name
+            string roomName = curRoom.get_Parameter(BuiltInParameter.ROOM_NAME).AsString();
+
+            // 7a. check room name
+            if (roomName.Contains("Office"))
+            {
+                TaskDialog.Show("Test", "Found the room!");
+            }
+
+            // 7.b get room point
+            Location roomLocation = curRoom.Location;
+            LocationPoint roomLocPt = curRoom.Location as LocationPoint;
+            XYZ roomPoint = roomLocPt.Point;
+
+
+            using Transaction t = new Transaction(doc);
+            t.Start("Insert Families into Rooms");
+
+            // 8. insert families
+            FamilySymbol familySymbol = GetFamilySymbolByName(doc, "Desk", "Large");
+            familySymbol.Activate();
+
+            foreach (Room curRoom2 in roomCollector)
+            {
+                LocationPoint loc = curRoom2.Location as LocationPoint;
+
+                FamilyInstance curFamInstance = doc.Create.NewFamilyInstance(loc.Point, familySymbol, curRoom2, StructuralType.NonStructural);
+            }
+
+            t.Commit();
+
+
+            return Result.Succeeded;
+        }
+
+        private FamilySymbol GetFamilySymbolByName(Document doc, string familyName, string familySymbolName)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfClass(typeof(FamilySymbol));
+
+            foreach (FamilySymbol familySymbol in collector)
+            {
+                if (familySymbol.Family.Name == familyName && familySymbol.Name == familySymbolName)
+                {
+                    return familySymbol;
+                }
+            }
+
+            return null;
+
+        }
+      
+    }
+
+    
+}
